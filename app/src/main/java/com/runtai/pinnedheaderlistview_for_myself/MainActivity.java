415e8ba2f02858ca34110a604636833d493a716e
rhf05290 <rhf05290@163.com>
2017-04-20 08:42:54 +0800

    Initial commit

---
+++ app/src/main/java/com/runtai/pinnedheaderlistview_for_myself/MainActivity.java
@@ -0,0 +1,105 @@
+package com.runtai.pinnedheaderlistview_for_myself;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.runtai.pinnedheaderlistview_for_myself.lib.PinnedHeaderListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends Activity {
+
+    private BladeView mLetter;
+    // 首字母集
+    private List<String> mSections;
+    // 根据首字母存放数据
+    private Map<String, List<String>> mMap;
+    // 首字母位置集
+    private List<Integer> mPositions;
+    // 首字母对应的位置
+    private Map<String, Integer> mIndexer;
+    private PinnedHeaderListView listView;
+    private String[] datas;
+    private static final String FORMAT = "^[a-z,A-Z].*$";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        listView = (PinnedHeaderListView) findViewById(R.id.pinnedListView);
+        LayoutInflater inflator = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout header1 = (LinearLayout) inflator.inflate(R.layout.total_header_layout, null);
+        ((TextView) header1.findViewById(R.id.tv_city_name)).setText("上海");
+
+        listView.addHeaderView(header1);
+        TestSectionedAdapter sectionedAdapter = new TestSectionedAdapter();
+        listView.setAdapter(sectionedAdapter);
+
+        mLetter = (BladeView) findViewById(R.id.mLetterListView);
+        mLetter.setOnItemClickListener(new BladeView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(String s) {
+                if (mIndexer.get(s) != null) {
+                    listView.setSelection(mIndexer.get(s));
+                }
+            }
+        });
+    }
+
+
+    private void initData() {
+        datas = getResources().getStringArray(R.array.countries);
+        mSections = new ArrayList<String>();
+        mMap = new HashMap<String, List<String>>();
+        mPositions = new ArrayList<Integer>();
+        mIndexer = new HashMap<String, Integer>();
+
+        for (int i = 0; i < datas.length; i++) {
+            String firstName = datas[i].substring(0, 1);
+            if (firstName.matches(FORMAT)) {
+                if (mSections.contains(firstName)) {
+                    mMap.get(firstName).add(datas[i]);
+                } else {
+                    mSections.add(firstName);
+                    List<String> list = new ArrayList<String>();
+                    list.add(datas[i]);
+                    mMap.put(firstName, list);
+                }
+            } else {
+                if (mSections.contains("#")) {
+                    mMap.get("#").add(datas[i]);
+                } else {
+                    mSections.add("#");
+                    List<String> list = new ArrayList<String>();
+                    list.add(datas[i]);
+                    mMap.put("#", list);
+                }
+            }
+        }
+
+        Collections.sort(mSections);
+        int position = 0;
+        for (int i = 0; i < mSections.size(); i++) {
+            mIndexer.put(mSections.get(i), position);// 存入map中，key为首字母字符串，value为首字母在listview中位置
+            mPositions.add(position);// 首字母在listview中位置，存入list中
+            position += mMap.get(mSections.get(i)).size();// 计算下一个首字母在listview的位置
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
